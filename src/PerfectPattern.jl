32e79ea4f6ae90ed462dd2c71134c05f83651c0a
thautawarm <twshere@outlook.com>
2020-04-25 21:27:04 +0900

    Files generated by PkgTemplates

---
+++ src/PerfectPattern.jl
@@ -0,0 +1,5 @@
+module PerfectPattern
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PerfectPattern
+using Test
+
+@testset "PerfectPattern.jl" begin
+    # Write your own tests here.
+end
